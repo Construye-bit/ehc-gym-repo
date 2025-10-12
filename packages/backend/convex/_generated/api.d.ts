1d37b0a1ffe50864f7c91018c1e278d4cd28e309
Diego Tenjo <tenjodiego961@gmail.com>
2025-10-11 23:53:32 -0500

    feat: add test utility imports to API module

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -13,6 +13,9 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as __tests___test_utils_builders from "../__tests__/test_utils/builders.js";
+import type * as __tests___test_utils_fakeCtx from "../__tests__/test_utils/fakeCtx.js";
+import type * as __tests___test_utils_run from "../__tests__/test_utils/run.js";
 import type * as addresses_errors from "../addresses/errors.js";
 import type * as addresses_mutations from "../addresses/mutations.js";
 import type * as addresses_queries from "../addresses/queries.js";
@@ -73,6 +76,9 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  "__tests__/test_utils/builders": typeof __tests___test_utils_builders;
+  "__tests__/test_utils/fakeCtx": typeof __tests___test_utils_fakeCtx;
+  "__tests__/test_utils/run": typeof __tests___test_utils_run;
   "addresses/errors": typeof addresses_errors;
   "addresses/mutations": typeof addresses_mutations;
   "addresses/queries": typeof addresses_queries;
